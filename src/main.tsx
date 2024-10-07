2dbb3dc6b29d01b4d5ebeb24b4b4237e959bad75
MarioBR04 <114209559+MarioBR04@users.noreply.github.com>
2024-10-06 23:42:54 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './Game';  // Asegúrate de que Game está bien importado
+import './index.css';  // Opcional si tienes algún CSS para estilos globales
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Game />  {/* Aquí renderizas el componente Game */}
+  </React.StrictMode>
+);
